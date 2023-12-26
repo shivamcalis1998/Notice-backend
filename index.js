513cd182a09f424c852490653c07436cf36c7eb5
unknown <shivamswami8899@gmail.com>
2023-12-27 01:14:34 +0530

    cors done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
+import cors from "cors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import authRoutes from "./routes/authRoutes.js";
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/auth", authRoutes);
 app.use("/notices", noticeRoutes);
 
